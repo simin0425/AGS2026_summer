71777ef157ef02282841014e552b834b0f1929ab
ASOST\2416044 <2416044@s.asojuku.ac.jp>
2026-04-27 14:41:02 +0900

    Playerクラスの整備

--- Files/src/Object/Player.h
+++ Files/src/Object/Player.h
@@ -12,7 +12,9 @@ public:
 	virtual void Draw() override;
 
 private:
-	static constexpr float MOVE_SPEED = 6.0f;
+	static constexpr float MOVE_SPEED = 6.0f * 60.0f;
+	static constexpr int PLAYER_NUM_MIN = 1;
+	static constexpr int PLAYER_NUM_MAX = 2;
 
 	const int PLAYER_NUM;
 
